de939ddc464df493b12a5269289eaccb79c9eed8
BoWei <as31288151jk@gmail.com>
2022-05-18 20:32:51 +0800

    表格連接json

---
+++ src/APIreader/style.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Div0 = styled.div`
+    overflow:auto;
+    height:100%;
+    width:100%;
+
+`
+export const Table0=styled.table`
+    margin-top:0%;
+    font-size:20px;
+`
+export const Thead0=styled.thead`
+    border-bottom:2px solid #8E8E8E;
+`
+export const Th0=styled.th`
+    width:1%;
+    border-bottom:1px solid #E0E0E0;
+    padding-bottom:9px;
+    padding-top:9px;
+    &.th1{
+        border-left:2px solid #8E8E8E;
+    }
+`
+export const Tr0=styled.th`
+    margin-top:2px;
+`
