316d5b815ca738f12d7455e130ebadf65befe7a8
azhe <10846006@ntub.edu.tw>
2022-11-29 10:20:56 +0800

    優化個人查詢頁面

--- src/new/common/dataDrawList/index.js
+++ src/new/common/dataDrawList/index.js
@@ -1,165 +1,138 @@
 //測試個人
 
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    Filler
+} from 'chart.js';
 import { Line } from "react-chartjs-2";
-import Chart from 'chart.js/auto';
-
 import { Div0, DataDrawItem, Table0, Thead0, Th0 } from './sty';
 import { getAuthSearchName } from "../apiUtil";
 import DataDrawListTrainB from "../dataDrawListTrainB";
 import { BACKEND_HOST } from "../../../global";
 
-var a = [];
-var stimes = [];
-var bicepstimes = [];
-let userNameSearch = getAuthSearchName();
-if (!userNameSearch) {
-    userNameSearch = "/admin";
-}
-var dataB = {
-    labels: a,
-    datasets: [
-        {
-            label: "二頭",
-            data: [],
-            fill: true,
-            backgroundColor: "rgba(75,192,192,0.2)",
-            borderColor: "rgba(75,192,192,1)",
-            lineTension: 0.4
-        },
-        {
-            label: "下肢",
-            data: [],
-            fill: true,
-            backgroundColor: "rgba(75,192,192,0.2)",
-            borderColor: "rgba(75,192,192,1)",
-            lineTension: 0.4
-        }
-    ]
-};
-class DataDrawList extends Component {
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+    Filler
+);
 
-    constructor(props) {
-        super(props);
-        // console.log(props);
-        this.state = {
-            "data": []
-        };
-    }
-    componentDidMount() {
-        this.getItems();
-    }
+const DataDrawList = () => {
 
-    getItems() {
-        fetch(`${BACKEND_HOST}/record/${userNameSearch}`, {
-            method: "GET",
-            credentials: 'include',
-            headers: new Headers({
-                'Content-Type': 'application/json',
-                // 'token': getAuthToken(), /* 把token放在這 */
-            })
-        })
-        .then(results => results.json())
-        .then(results => { this.setState({ "data": results.data }) });
-    }
+    const [records, setRecords] = useState([]);
+    const [bicepsRecords, setBicepsRecords] = useState([]);
+    const [quadricepsRecords, setQuadricepsRecords] = useState([]);
+    const [chartLabels, setChartLabels] = useState([]);
 
-    render() {
-        userNameSearch = getAuthSearchName();
-        // console.log('1'+userNameSearch);
-        // userNameSearch =window.location.href;
-        // userNameSearch=userNameSearch.replace("http://localhost:3000/personal","")
-        fetch(`${BACKEND_HOST}/record/${userNameSearch}`, {
-            method: "GET",
-            credentials: 'include',
-            headers: new Headers({
-                'Content-Type': 'application/json',
-                // 'token': getAuthToken(), /* 把token放在這 */
-            })
-        }).then(function (response) {
-                return response.json();
-            })
-            .then(function (myJson) {
-                // console.log(myJson.data);
-                return myJson.data;
-            })
-            .then(function (myData) {
-                bicepstimes = [];
-                stimes = [];
-                a = [];
-                // console.log(myData);
-                for (var i = 0; i < myData.length; i++) {
-                    if (myData[i].part === '二頭肌') {
-                        bicepstimes.push(myData[i].times);
-                        let arr1 = myData[i].create_time.split(' ');
-                        let arr = arr1[0].split('-');
-                        a.push(arr[1] + '/' + arr[2]);
+    useEffect(() => {
+        const fetchData = async () => {
+            var userNameSearch = getAuthSearchName();
+            if (!userNameSearch) {
+                userNameSearch = "/admin";
+            }
+
+            var result = await axios.get(
+                `${BACKEND_HOST}/record/${userNameSearch}`,
+                {
+                    withCredentials: true,
+                    headers: {
+                        'Content-Type': 'application/json',
                     }
-                    if (myData[i].part === '股四頭肌') {
-                        stimes.push(myData[i].times);
+                },
+            );         
+            let _records = result.data['data'];
+            setRecords(_records);
 
-                    }
+            var _bicepsRecords = [],
+                _quadricepsRecords = [],
+                _chartLabels = [];
+            
+            for (var i = 0; i < _records.length; i++) {
+                switch (_records[i].part) {
+                    case 0:
+                        _bicepsRecords.push(_records[i].times);
+                        let recordCreateTime = new Date(_records[i].create_time);
+                        _chartLabels.push(`${recordCreateTime.getMonth()+1}/${recordCreateTime.getDate()}`);
+                        break;
+                    case 2:
+                        _quadricepsRecords.push(_records[i].times);
+                        break;
+                    default:
+                        break;
                 }
-                //   console.log(bicepstimes);
-                dataB = {
+            }
+            setBicepsRecords(_bicepsRecords);
+            setQuadricepsRecords(_quadricepsRecords);
+            setChartLabels(_chartLabels);
+        };
+        fetchData();
+    }, []);
 
-                    labels: a,
-                    datasets: [
+    const chartData = {
+        labels: chartLabels,
+        datasets: [
+            {
+                label: "二頭肌",
+                data: bicepsRecords,
+                fill: true,
+                backgroundColor: "rgba(75,192,192,0.2)",
+                borderColor: "rgba(75,192,192,1)",
+                lineTension: 0.5
+            },
+            {
+                label: "下肢",
+                data: quadricepsRecords,
+                fill: true,
+                backgroundColor: "rgba(255,0,0,0.2)",
+                borderColor: "rgba(255,0,0,1)",
+                lineTension: 0.5
+            }
+        ]
+    };
+    
+    return (
+        <Div0>
+            <DataDrawItem>
+                <Line data={chartData} />
+            </DataDrawItem>
+
+            <DataDrawItem>
+                <Table0 className="table">
+                    <Thead0>
+                        <tr>
+                            <Th0 scope="col">日期</Th0>
+                            <Th0 className="th1" scope="col">部位</Th0>
+                            <Th0 className="th1" scope="col">次數</Th0>
+                        </tr>
+                    </Thead0>
+                    <tbody>
                         {
-                            label: "二頭肌",
-                            data: bicepstimes,
-                            fill: true,
-                            backgroundColor: "rgba(75,192,192,0.2)",
-                            borderColor: "rgba(75,192,192,1)",
-                            lineTension: 0.5
-                        },
-                        {
-                            label: "下肢",
-                            data: stimes,
-                            fill: true,
-                            backgroundColor: "rgba(255,0,0,0.2)",
-                            borderColor: "rgba(255,0,0,1)",
-                            lineTension: 0.5
+                            records.map((item, index) => (
+                                <tr key={index}>
+                                    <Th0>{item.create_time.split(" ")[0]}</Th0>
+                                    <Th0 className="th1">{item.part_name}</Th0>
+                                    <Th0 className="th1">{item.times}</Th0>
+                                </tr>
+                            ))
                         }
-
-                    ]
-
-
-
-                };
-
-            });
-        return (
-            <Div0>
-                <DataDrawItem>
-
-                    <Line data={dataB} />
-                </DataDrawItem>
-
-                <DataDrawItem>
-                    <Table0 className="table">
-                        <Thead0>
-                            <tr>
-                                <Th0 scope="col">日期</Th0>
-                                <Th0 className="th1" scope="col">部位</Th0>
-                                <Th0 className="th1" scope="col">次數</Th0>
-                            </tr>
-                        </Thead0>
-                        <tbody>
-                            {
-                                this.state.data.map((item, index) => (
-                                    <tr key={index}>
-                                        <Th0>{item.create_time.split(" ")[0]}</Th0>
-                                        <Th0 className="th1">{item.part}</Th0>
-                                        <Th0 className="th1">{item.times}</Th0>
-                                    </tr>
-                                ))
-                            }
-                        </tbody>
-                    </Table0>
-                </DataDrawItem>
-                <DataDrawItem><DataDrawListTrainB></DataDrawListTrainB></DataDrawItem>
-            </Div0>
-        )
-    }
+                    </tbody>
+                </Table0>
+            </DataDrawItem>
+            <DataDrawItem><DataDrawListTrainB></DataDrawListTrainB></DataDrawItem>
+        </Div0>
+    )
 }
 export default DataDrawList;
