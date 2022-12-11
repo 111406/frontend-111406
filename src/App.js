4a93c2288a7f77b6cca0b3a2ee275466dd90d0e5
azhe <10846006@ntub.edu.tw>
2022-12-11 18:14:20 +0800

    update BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component} from 'react'
+import React, { Component } from 'react'
 import './style.css'
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/login/index.js';
 import PersonalPage from './new/page/mainPage/index.js';
 import SearchPage from './new/page/searchpage/index.js';
@@ -8,19 +8,19 @@ import AllPage from './new/page/allPage';
 class App extends Component {
   render() {
     return (
-    <div className="App">
-        <BrowserRouter>
+      <div className="App">
+        <HashRouter>
           <Routes>
-            <Route path='/' element={<Login/>}></Route>
-            <Route path='/personal/:searchName' element={<PersonalPage/>}></Route>
-            <Route path='/search' element={<SearchPage/>}></Route>
-            <Route path='/allpage' element={<AllPage/>}></Route>
+            <Route path='/' element={<Login />}></Route>
+            <Route path='/personal/:searchName' element={<PersonalPage />}></Route>
+            <Route path='/search' element={<SearchPage />}></Route>
+            <Route path='/allpage' element={<AllPage />}></Route>
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
 
-    </div>
-  );
-}
+      </div>
+    );
+  }
 }
 
 export default App
