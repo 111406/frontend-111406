b1d64ff804c6ce0906c8949164788bf684e74c91
azhe <10846006@ntub.edu.tw>
2022-12-12 16:15:33 +0800

    新增錯誤訊息頁面

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Login from './pages/login/index.js';
 import PersonalPage from './new/page/mainPage/index.js';
 import SearchPage from './new/page/searchpage/index.js';
 import AllPage from './new/page/allPage';
+import PageNotFound from './error/404'
+import Unauthorized from './error/401'
+import Forbidden from './error/403'
 class App extends Component {
   render() {
     return (
@@ -15,6 +18,9 @@ class App extends Component {
             <Route path='/personal/:searchName' element={<PersonalPage />}></Route>
             <Route path='/search' element={<SearchPage />}></Route>
             <Route path='/allpage' element={<AllPage />}></Route>
+            <Route path='/*' element={<PageNotFound />}></Route>
+            <Route path='/401' element={<Unauthorized />}></Route>
+            <Route path='/403' element={<Forbidden />}></Route>
           </Routes>
         </HashRouter>
 
---
+++ src/error/403/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Forbidden = () => {
+    return (
+        <div>
+            <h2>403 Forbidden</h2>
+        </div>
+    );
+}
+export default Forbidden;
